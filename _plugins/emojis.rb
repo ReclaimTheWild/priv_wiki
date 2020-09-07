ca67d2291c28ab8235a6342a61fbc9f0359fb719
MeixDev <main.meixdev@gmail.com>
2020-09-07 05:52:55 +0200

    Test custom emoji tag

---
+++ _plugins/emojis.rb
@@ -0,0 +1,13 @@
+class ZeldaEmojis < Liquid::Tag
+    def initialize(tag_name, input, tokens)
+      super
+      @input = input
+    end
+  
+    def render(context)
+      output = "<img src=\"/assets/images/emoji/{input}.png\" alt=\"{input}\">"
+      return output;
+    end
+  end
+  Liquid::Template.register_tag('emoji', ZeldaEmojis)
+  Liquid::Template.register_tag('em', ZeldaEmojis)
--- index.md
+++ index.md
@@ -9,6 +9,8 @@ nav_order: 1
 
 ![twili_icon](assets/images/emoji/twili.png) I am a Twili
 
+{%- em twili %} {%- emoji twili %} Test emoji
+
 Welcome to the mirror of Reclaim the Wild's homebrew wiki !
 
 To contribute, please refer to the [Contributing](contributing) page. You can also get informations about the syntax of the pages [here](syntax_templates).
